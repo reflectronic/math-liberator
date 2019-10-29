d37a1a31a36507f01607f7692f59cd9f30d4fd2f
reflectronic <john-tur@outlook.com>
2019-10-29 15:13:02 -0400

    Add project files

---
+++ MathLiberator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathLiberator
+{
+    static class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
